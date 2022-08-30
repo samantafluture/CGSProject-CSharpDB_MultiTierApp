8e304ec55cfbf7860abbbc9349bd1add0aa12103
Samanta Fluture <samantafluture@gmail.com>
2022-08-30 14:25:01 -0400

    Add project files.

---
+++ Curator.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CGS
+{
+    internal class Curator : Person
+    {
+        const double commRate = 0.1; 
+        string curatorID;
+        double commission;
+
+        public Curator(string firstName, string lastName, string curatorID, double commision) 
+            : base(firstName, lastName)
+        {
+            this.curatorID = curatorID;
+            this.commission = commision;
+        }
+
+        public string CuratorID
+        {
+            get { return curatorID; }
+            set { curatorID = value; }
+        }
+
+        public double Commission
+        {
+            get { return commission; }
+            set { commission = value; }
+        }
+
+        public string GetID()
+        {
+            return CuratorID;
+        }
+
+        public void SetComm(double newCommission)
+        {
+            Commission += newCommission;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() + " " + CuratorID + " " + Commission;
+        }
+    }
+}
---
+++ Person.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CGS
+{
+    // abstract -> we do not instantiated, they are meant to be inherent
+    abstract class Person
+    {
+        // default -> is private 
+        // local variables
+        string firstName;
+        string lastName;
+
+        // constructor -> method to initialize the local variables (same name of the class)
+        // receive arguments / paramenters
+        // this -> refers to the local variables
+        public Person(string firstName, string lastName)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+        }
+
+        // get and set methods
+        public string FirstName 
+        { 
+            get 
+            { 
+                return firstName; 
+            } 
+            set 
+            { 
+                firstName = value; 
+            } 
+        }
+
+        public string LastName 
+        {
+            get 
+            { 
+                return lastName; 
+            } 
+            set 
+            { 
+                lastName = value; 
+            } 
+        }
+
+        // use properties instead of local variables this.firstName...
+        // override a method that is inhenrit 
+        public override string ToString()
+        {
+            return FirstName + " " + LastName;
+        }
+    }
+}
