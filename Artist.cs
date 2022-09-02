b28dac5353ec76165e1690e9e1a1b688a38cf8b2
Samanta Fluture <samantafluture@gmail.com>
2022-09-02 15:32:12 -0400

    add artist class

---
+++ Artist.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CGS
+{
+    class Artist : Person
+    {
+        string artistID;
+
+        public Artist(string artistID, string firstName,
+            string lastName) : base(firstName, lastName)
+        {
+            this.artistID = artistID;
+        }
+
+        public string ArtistID
+        {
+            get { return artistID; }
+            set { artistID = value; }
+        }
+
+        public override string toString()
+        {
+            return ArtistID + " " + base.toString();
+        }
+
+        public string getID()
+        {
+            return artistID;
+        }
+    }
+}
