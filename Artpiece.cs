5a7ab2f46c7caaf3b61819d35ec9cde4df7a4cdb
Samanta Fluture <samantafluture@gmail.com>
2022-09-02 17:45:59 -0400

    interface curators for data stratucture

---
+++ Artpiece.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CGS
+{
+    // when it's nothing before class, it's private
+    // by default, everything is private
+    class Artpiece
+    {
+        string pieceID, title, artistID, curatorID;
+        int year;
+        double value, estimate; // value = price; estimate = offer
+        char status;
+
+        public Artpiece(string pieceID, string title, int year, double value,
+            double estimate, char status, string artistID, string curatorID)
+        {
+            this.pieceID = pieceID;
+            this.title = title;
+            this.year = year;
+            this.value = value;
+            this.estimate = estimate;
+            this.status = status;
+            this.artistID = artistID;
+            this.curatorID = curatorID;
+        }
+
+        public string PieceID
+        {
+            get { return pieceID; }
+            set { pieceID = value; }
+        }
+
+        public string Title
+        {
+            get { return title; }
+            set { title = value; }
+        }
+
+        public int Year
+        {
+            get { return year;  }
+            set { year = value;  }
+        }
+
+        public double Value
+        {
+            get { return value; }
+            set { this.value = value; }
+        }
+
+        public double Estimate
+        {
+            get { return estimate; }
+            set { estimate = value; }
+        }
+
+        public char Status
+        {
+            get { return status; }
+            set { status = value; }
+        }
+
+        public string ArtistID
+        {
+            get { return artistID; }
+            set { artistID = value; }
+        }
+
+        public string CuratorID
+        {
+            get { return curatorID; }
+            set { curatorID = value; }
+        }
+
+
+        public string GetId()
+        {
+            return PieceID;
+        }
+
+        public override string ToString()
+        {
+            return PieceID + " " + Title + " " + this.curatorID + " " + this.artistID + 
+                " " + Year + " " + Value + " " + Estimate + " " + Status;
+        }
+
+        public void ChangeStatus(char stat)
+        {
+            Status = stat;
+        }
+
+        public double CalculateComm(double val, double est)
+        {
+            return (est - val) * 0.10;
+        }
+    }
+}
---
+++ Curators.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CGS
+{
+    // inherets from a coletor of data structure / colletion base
+    // collection base is a package for data structure
+    // in that way, I can call list
+    // we add an object into the list
+    class Curators : CollectionBase
+    {
+        public void Add(Curator curator)
+        {
+            List.Add(curator);
+        }
+
+        // special method; get access to access of list
+        // it's like a "casting" of the class Curators 
+        public Curator this[int index]
+        {
+            // type casting, changing the type of a variable if it is necessary for the result
+            get { return (Curator)List[index]; } 
+            set { List[index] = value;}
+        }
+    }
+}
