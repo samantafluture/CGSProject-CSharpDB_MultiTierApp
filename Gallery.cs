40aad941997bf57e8d258d582c72e1e74d6f61e5
Samanta Fluture <samantafluture@gmail.com>
2022-09-09 15:20:36 -0400

    correct methods of add curator and add artist

--- Gallery.cs
+++ Gallery.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -29,34 +28,41 @@ namespace CGS
             }
             else
             {
-                if (CuratorVerifier(curID) == true)
+                // id -> check if id already exists in the list (uniqueness of ID)
+                // myCurators -> object list for the class Curators
+                foreach (Curator cur in myCurators)
                 {
-                    Console.WriteLine("Error! This id already exists");
+                    if (cur.GetID() == curID)
+                    {
+                        Console.WriteLine("Error! There is another curator with this ID");
+                        return;
+                        // return if there's another curator with this ID (or the condition is false)
+                        // break is used to exit (escape) the loop that you are currently executing
+                        // return will exit the entire method you are currently executings
+                    }
+
+                }
+                Console.WriteLine("Please enter the first name: ");
+                string fn = Console.ReadLine();
+                Console.WriteLine("Please enter the last name: ");
+                string ln = Console.ReadLine();
+
+                // fn, ln -> check for the lenght
+                if (fn.Length + ln.Length > 40)
+                {
+                    Console.WriteLine("Error! The lenght of full name should be less than 40 characters");
                 }
                 else
                 {
-                    Console.WriteLine("Please enter the first name: ");
-                    string fn = Console.ReadLine();
-                    Console.WriteLine("Please enter the last name: ");
-                    string ln = Console.ReadLine();
+                    double commission = 0.0;
 
-                    // fn, ln -> check for the lenght
-                    if (fn.Length + ln.Length > 40)
-                    {
-                        Console.WriteLine("Error! The lenght of full name should be less than 40 characters");
-                    }
-                    else
-                    {
-                        double commission = 0.0;
+                    // instantiate the class of curator
+                    Curator cur = new Curator(curID, commission, fn, ln);
 
-                        // instantiate the class of curator
-                        Curator cur = new Curator(curID, commission, fn, ln);
-
-                        // add the current object to the list
-                        // object is a copy of the class
-                        myCurators.Add(cur);
-                        Console.WriteLine("The curator has been succefully added to the list");
-                    }
+                    // add the current object to the list
+                    // object is a copy of the class
+                    myCurators.Add(cur);
+                    Console.WriteLine("The curator has been succefully added to the list");
                 }
             }
         }
@@ -72,28 +78,31 @@ namespace CGS
             }
             else
             {
-                if (ArtistVerifier(aID) == true)
+                foreach (Artist artist in myArtists)
                 {
-                    Console.WriteLine("Error! This id already exists");
-                } else
+                    if (artist.GetID() == aID)
+                    {
+                        Console.WriteLine("Error! There is another artist with this ID");
+                        return;
+                    }
+                }
+
+                Console.WriteLine("Please enter the first name: ");
+                string fn = Console.ReadLine();
+                Console.WriteLine("Please enter the last name: ");
+                string ln = Console.ReadLine();
+
+                if (fn.Length + ln.Length > 40)
                 {
-                    Console.WriteLine("Please enter the first name: ");
-                    string fn = Console.ReadLine();
-                    Console.WriteLine("Please enter the last name: ");
-                    string ln = Console.ReadLine();
+                    Console.WriteLine("Error! The lenght of full name should be less than 40 characters");
+                }
+                else
+                {
+                    Artist artist = new Artist(aID, fn, ln);
+                    myArtists.Add(artist);
 
-                    if (fn.Length + ln.Length > 40)
-                    {
-                        Console.WriteLine("Error! The lenght of full name should be less than 40 characters");
-                    }
-                    else
-                    {
-                        Artist artist = new Artist(aID, fn, ln);
-                        myArtists.Add(artist);
-
-                        Console.WriteLine("The artist has been succefully added to the list");
-                    }
-                } 
+                    Console.WriteLine("The artist has been succefully added to the list");
+                }
             }
         }
 
@@ -145,7 +154,7 @@ namespace CGS
                             double estimate = 0.0;
                             char status = 'E';
 
-                            Artpiece artpiece = new Artpiece(pID, title, year, value,
+                            Artpiece artpiece = new Artpiece(pID, title, year, value, 
                                 estimate, status, aID, cID);
                             myArtpieces.Add(artpiece);
 
@@ -156,15 +165,15 @@ namespace CGS
 
             }
         }
-
+        
         // check if artpiece ID exists (if is valid)
         public bool ArtpieceVerifier(string artPId)
         {
             bool flag = false;
 
-            foreach (Artpiece piece in myArtpieces)
+            foreach(Artpiece piece in myArtpieces)
             {
-                if (piece.GetId() == artPId)
+                if(piece.GetId() == artPId)
                 {
                     flag = true;
                 }
